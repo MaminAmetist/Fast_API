d4ad133e8bf2c05673ddcbc4ad4eed00a5e068a2
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-11-15 20:29:04 +0700

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome() -> dict:
+    return {'message': 'Главная страница.'}
+
+
+@app.get('/user/admin')
+async def admin() -> dict:
+    return {'message': 'Вы вошли как администратор.'}
+
+
+@app.get('/user/{user_id}')
+async def id_user(user_id: int) -> dict:
+    return {'message': f'Вы вошли как пользователь № {user_id}.'}
+
+
+@app.get('/user')
+async def user_info(username: str = 'Anna', age: int = 40):
+    return {'message': f'Информация о пользователе. Имя: {username}. Возраст: {age}'}
