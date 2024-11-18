1ddf124dbfa1897bd679534750f6b4b8362e81bd
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-11-18 16:45:03 +0700

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,49 @@
+# Модели данных Pydantic
+# Задача "Модель пользователя":
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+@app.get('/users')
+async def get_all_users() -> List[User]:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def create_user(username: str, age: int) -> User:
+    user_id = len(users) + 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: int, username: str, age: int) -> List[User]:
+    try:
+        edit_user = users[user_id - 1]
+        edit_user.username = username
+        edit_user.age = age
+        return users
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int) -> List[User]:
+    del_user = users[user_id - 1]
+    if del_user.id == user_id:
+        users.remove(del_user)
+        return users
+    else:
+        raise HTTPException(status_code=404, detail='User was not found')
