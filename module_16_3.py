00814b1f3d61997a19129a87edc8197030ef0f55
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-11-18 09:34:44 +0700

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -7,7 +7,7 @@ app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
-@app.get('/')
+@app.get('/users')
 async def get_all_users() -> dict:
     return users
 
@@ -15,13 +15,13 @@ async def get_all_users() -> dict:
 @app.post('/user/{username}/{age}')
 async def create_user(username: str, age: str) -> str:
     user_id = str(int(max(users, key=int)) + 1)
-    users[user_id] = [f'Имя: {username}, возраст: {age}']
+    users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} is registered.'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: str, username: str, age: str) -> str:
-    users[user_id] = [f'Имя: {username}, возраст: {age}']
+    users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'The user {user_id} is updated.'
 
 
